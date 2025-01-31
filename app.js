6fbca84027820fc3cec9b6ad82c883a9cf575d15
Adeeba-Khan/jsProject <adeebacs0008@gmail.com>
2025-01-31 21:23:51 +0530

    express1/adeeba-khan

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 
-const http = require('http');
-const fs = require('fs');
-const routes= require('./routes');
-const server = http.createServer(routes.handler);
-console.log(routes.someText);
-server.listen(3000, () => {
+// const http = require('http');
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+    console.log("In the middleware");
+    next();
+});
+app.use((req,res,next)=>{
+    console.log("In the another middleware");
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+
+// const fs = require('fs');
+// const routes= require('./routes');
+// const server = http.createServer(routes.handler);
+// console.log(routes.someText);
+
+app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
