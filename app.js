91ca747c4987ed0e5206b6979e8470f11363932c
Adeeba-Khan/jsProject <adeebacs0008@gmail.com>
2025-02-03 22:54:02 +0530

    express2/adeeba-khan

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 
 // const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req,res,next)=>{
-    console.log("In the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req,res,next)=>{
+ console.log('In the middleware!');
+ res.send('<form action="product" method="post"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>');
+
 });
+
+app.use('/product',(req,res,next)=>{
+    console.log("body",req.body.title);
+    console.log("size",req.body.size);
+
+    res.redirect('/');
+})
+// app.use((req,res,next)=>{
+//     console.log("In the middleware");
+//     next();
+// });
 app.use((req,res,next)=>{
     console.log("In the another middleware");
     res.send('<h1>Hello from Express!</h1>');
 });
 
 
-// const fs = require('fs');
-// const routes= require('./routes');
-// const server = http.createServer(routes.handler);
-// console.log(routes.someText);
 
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
